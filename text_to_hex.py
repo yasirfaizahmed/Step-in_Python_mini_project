1cea4cef0de24e3df618c5c17b2d2ef0200640b8
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2021-07-13 20:55:05 +0530

    all done with .txt to .hex

--- text_to_hex.py
+++ text_to_hex.py
@@ -1,47 +1,43 @@
 
-#### this module converts .txt file to .hex format 
-dependentfilename = "myfile.txt"    #our depedency file
-try:
-    file_txt = open(dependentfilename, 'r')
-except:
-    print("could not find file Named ", dependentfilename,"\n")
-    exit(0)
-
-
-
-
-targetfilename = "file_hex.hex"     #our target file
-
 def main():
     print("\n\t\t\t\tWelcome, this is a file format converter, \n\t\t\tplease enter the dependency file name along with its extension")
     print("\t\t----------------------------------------------------------------------------------\n\n")
-    depdendency_file_name = input("\t\tEnter here: ")
+    dependency_file_name = input("\t\tEnter here: ")
     target_file_name = input("\n\t\tEnter here: ")
-    depdendency_file_extension = depdendency_file_name[(depdendency_file_name.find('.')+1) : ]  #here we are detecting the extension of the dependency file
-    print(depdendency_file_extension)
+    dependency_file_extension = dependency_file_name[(dependency_file_name.find('.')) : ]  #seperating extensions
+    target_file_extension = target_file_name[(target_file_name.find('.')) : ]     #seperating extensions
+
+    try:
+        dependency_file_obj = open(dependency_file_name, 'r')
+    except:
+        print("\t\tcould not find file named\n", dependency_file_name)
+
+
+    if dependency_file_extension == ".txt" and target_file_extension == ".hex":
+        txt_to_hex(dependency_file_obj, target_file_name)
+
  
     
 
-def txt_to_hex(file_txt, targetfilename):
+def txt_to_hex(dependency_file_obj, target_file_name):
     try:
-        file_hex = open(targetfilename, 'x')    #creating a file for the first time
+        hex_file = open(target_file_name, 'x')    #creating a file for the first time
     except:
         try:
-            file_hex = open(targetfilename, 'w')    #if the file already exists
+            hex_file = open(target_file_name, 'w')    #if the file already exists
         except:
-            print("could not find file named", targetfilename, "\n")
+            print("could not find file named", target_file_name, "\n")
             exit(0)
 
     val = 0x00  
-    for lines in file_txt:
+    for lines in dependency_file_obj:
         for chrs in lines:
             val = hex(ord(chrs))[2:]    #converting each charecter to ASCII value, then to hex value, and chopping the '0x'
-            file_hex.write(val+" ")
-    file_hex.close()
-    file_txt.close()
+            hex_file.write(val+" ")
+    hex_file.close()
+    dependency_file_obj.close()
 
 
 if __name__ == "__main__":
     main()
-    txt_to_hex(file_txt, targetfilename)
 
