39469475a31a9561b80c28a52691f25c21b76f05
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2021-07-13 18:02:22 +0530

    first commit, .txt-.hex works well

---
+++ text_to_hex.py
@@ -0,0 +1,31 @@
+
+#### this module converts .txt file to .hex format 
+dependentfilename = "myfile.txt"    #our file of depedency
+try:
+    file_txt = open(dependentfilename, 'r')
+except:
+    print("could not find file Named ", dependentfilename,"\n")
+    exit(0)
+
+
+targetfilename = "file_hex.hex"     #our file of target
+try:
+    file_hex = open(targetfilename, 'x')    #creating a file for the first time
+except:
+    try:
+        file_hex = open(targetfilename, 'w')    #if the file already exists
+    except:
+        print("could not find file named", targetfilename, "\n")
+        exit(0)
+
+    
+
+val = 0x00  
+for lines in file_txt:
+    for chrs in lines:
+        val = hex(ord(chrs))[2:]    #converting each charecter to ASCII value, then to hex value, and chopping the '0x'
+        file_hex.write(val+" ")
+file_hex.close()
+file_txt.close()
+
+
