402d036fea75716c377bb3f162c5caf0337009d7
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2021-07-13 18:23:41 +0530

    some changes

--- text_to_hex.py
+++ text_to_hex.py
@@ -9,23 +9,28 @@ except:
 
 
 targetfilename = "file_hex.hex"     #our file of target
-try:
-    file_hex = open(targetfilename, 'x')    #creating a file for the first time
-except:
-    try:
-        file_hex = open(targetfilename, 'w')    #if the file already exists
-    except:
-        print("could not find file named", targetfilename, "\n")
-        exit(0)
+
 
     
+def txt_to_hex(file_txt, targetfilename):
+    try:
+        file_hex = open(targetfilename, 'x')    #creating a file for the first time
+    except:
+        try:
+            file_hex = open(targetfilename, 'w')    #if the file already exists
+        except:
+            print("could not find file named", targetfilename, "\n")
+            exit(0)
 
-val = 0x00  
-for lines in file_txt:
-    for chrs in lines:
-        val = hex(ord(chrs))[2:]    #converting each charecter to ASCII value, then to hex value, and chopping the '0x'
-        file_hex.write(val+" ")
-file_hex.close()
-file_txt.close()
+    val = 0x00  
+    for lines in file_txt:
+        for chrs in lines:
+            val = hex(ord(chrs))[2:]    #converting each charecter to ASCII value, then to hex value, and chopping the '0x'
+            file_hex.write(val+" ")
+    file_hex.close()
+    file_txt.close()
 
+
+
+txt_to_hex(file_txt, targetfilename)
 
