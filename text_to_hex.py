39689e3b2e88f29984d9972fd6ee99e9656212ba
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2021-07-13 21:55:15 +0530

    can convert .txt to .bin now

--- text_to_hex.py
+++ text_to_hex.py
@@ -7,35 +7,57 @@ def main():
     dependency_file_extension = dependency_file_name[(dependency_file_name.find('.')) : ]  #seperating extensions
     target_file_extension = target_file_name[(target_file_name.find('.')) : ]     #seperating extensions
 
+    #Depedency file object creation
     try:
         dependency_file_obj = open(dependency_file_name, 'r')
     except:
         print("\t\tcould not find file named\n", dependency_file_name)
-
-
-    if dependency_file_extension == ".txt" and target_file_extension == ".hex":
-        txt_to_hex(dependency_file_obj, target_file_name)
-
- 
     
-
-def txt_to_hex(dependency_file_obj, target_file_name):
+    #target file object creation
     try:
-        hex_file = open(target_file_name, 'x')    #creating a file for the first time
+        target_file_obj = open(target_file_name, 'x')    #creating a file for the first time
     except:
         try:
-            hex_file = open(target_file_name, 'w')    #if the file already exists
+            target_file_obj = open(target_file_name, 'w')    #if the file already exists
         except:
             print("could not find file named", target_file_name, "\n")
             exit(0)
 
+
+    if dependency_file_extension == ".txt" and target_file_extension == ".hex":
+        txt_to_hex(dependency_file_obj, target_file_obj)
+    if dependency_file_extension == ".txt" and target_file_extension == ".bin":
+        txt_to_bin(dependency_file_obj, target_file_obj)
+
+ 
+    
+
+def txt_to_hex(dependency_file_obj, target_file_obj):
     val = 0x00  
     for lines in dependency_file_obj:
         for chrs in lines:
             val = hex(ord(chrs))[2:]    #converting each charecter to ASCII value, then to hex value, and chopping the '0x'
-            hex_file.write(val+" ")
-    hex_file.close()
+            target_file_obj.write(val+" ")
+    target_file_obj.close()
     dependency_file_obj.close()
+
+    print("\n\n\t\t your .txt file has been converted to .hex file\n\n")
+
+def txt_to_bin(dependency_file_obj, target_file_obj):
+    val = 0
+    for lines in dependency_file_obj:
+        for chrs in lines:
+            val = bin(ord(chrs))[2:]
+            if len(val)<8:
+               for i in range(8-len(val)):
+                   val = '0' + val
+            target_file_obj.write(val + " ")
+    target_file_obj.close()
+    dependency_file_obj.close()
+
+    print("\n\n\t\t your .txt file has been converted to .bin file\n\n")
+
+
 
 
 if __name__ == "__main__":
