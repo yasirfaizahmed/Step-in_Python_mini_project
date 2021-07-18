7d7089eef08918c4a8326b9d777b71832f3f24e4
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2021-07-18 21:13:35 +0530

    added bin test

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ import os
 #sys.path.insert(1, "C:\\Users\YASIR\Documents\STEPin Projects\Python_mini_project\src")
 import main
 
-
+#definifn all the test fucntions here
 def test_txt_to_hex():
     test_output_file = open("test/test_hex.hex", 'x')  #generating a hex file to get it tested 
     test_input_file = open("test/template_myfile.txt", 'r') #our tepmplate txt file
@@ -11,17 +11,32 @@ def test_txt_to_hex():
 
     main.txt_to_hex(test_input_file, test_output_file)  #gets generated here
     
-    test_output_file = open("test/test_hex.hex", 'r')
-    template_file = open("test/template_hex.hex", 'r')
+    test_output_file = open("test/test_hex.hex", 'r')   #opening again 
+    template_file = open("test/template_hex.hex", 'r')  #opening again
     for f, b in zip(template_file, test_output_file):   #testing here
         assert f == b
     test_output_file.close()
     template_file.close()
 
-
     os.remove("test/test_hex.hex")  #deleting the tested files
 
+def test_txt_to_bin():
+    test_output_file = open("test/test_bin.bin", 'x')
+    test_input_file = open("test/template_myfile.txt", 'r')
+    template_file = open("test/template_bin.bin", 'r')
 
-test_txt_to_hex()
+    main.txt_to_bin(test_input_file, test_output_file)
+
+    test_output_file = open("test/test_bin.bin", 'r')
+    template_file = open("test/template_bin.bin", 'r')
+    for f, b in zip(template_file, test_output_file):
+        assert f == b
+    test_output_file.close()
+    template_file.close()
+
+    os.remove("test/test_bin.bin")
+
+
+
     
 
